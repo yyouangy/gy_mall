b38bf098e686c9545bf64139fcff87977e4a7911
yyouangy <kk450512574@163.com>
2020-06-08 23:53:19 +0800

    category

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -1,24 +1,103 @@
 <template>
-  <div class="content">
-   
-    
+  <div class="wrapper">
+    <ul class="content">
+      <button @click="btnClick">按钮</button>
+      <li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li>
+      <li>列表4</li>
+      <li>列表5</li>
+      <li>列表6</li>
+      <li>列表7</li>
+      <li>列表8</li>
+      <li>列表9</li>
+      <li>列表10</li>
+      <li>列表11</li>
+      <li>列表12</li>
+      <li>列表13</li>
+      <li>列表14</li>
+      <li>列表15</li>
+      <li>列表16</li>
+      <li>列表17</li>
+      <li>列表18</li>
+      <li>列表19</li>
+      <li>列表20</li>
+      <li>列表21</li>
+      <li>列表22</li>
+      <li>列表23</li>
+      <li>列表24</li>
+      <li>列表25</li>
+      <li>列表26</li>
+      <li>列表27</li>
+      <li>列表28</li>
+      <li>列表29</li>
+      <li>列表30</li>
+      <li>列表31</li>
+      <li>列表32</li>
+      <li>列表33</li>
+      <li>列表34</li>
+      <li>列表35</li>
+      <li>列表36</li>
+      <li>列表37</li>
+      <li>列表38</li>
+      <li>列表39</li>
+      <li>列表40</li>
+      <li>列表41</li>
+      <li>列表42</li>
+      <li>列表43</li>
+      <li>列表44</li>
+      <li>列表45</li>
+      <li>列表46</li>
+      <li>列表47</li>
+      <li>列表48</li>
+      <li>列表49</li>
+      <li>列表50</li>
+    </ul>
   </div>
 </template>
 
-
 <script>
-import BScroll from 'better-scroll'
+  import BScroll from 'better-scroll'
 
-export default {
-  name:"Category",
-
-}
+  export default {
+    name: "category",
+    data() {
+      return {
+        scroll: null
+      }
+    },
+    methods: {
+      btnClick() {
+        console.log('按钮被点击');
+      }
+    },
+    mounted() {
+      this.scroll = new BScroll(document.querySelector('.wrapper'), {
+        probeType: 3,
+        // 最新版本支持监听原生click
+        // click: false,
+        pullUpLoad: true
+      });
+      this.scroll.on('scroll', (position) => {
+        // console.log(position);
+      });
+      this.scroll.on('pullingUp', () => {
+        //网络请求操作
+        setTimeout(() => {
+          console.log('上拉加载数据');
+          this.scroll.finishPullUp();
+        }, 2000)
+      })
+    }
+  }
 </script>
+
 <style scoped>
-/* 原生js实现局部窗口滚动
-.content{
-    height: 150px;
-    background-color: skyblue;
-    overflow-y:scroll;
-} */
+  .wrapper {
+    height: 400px;
+    background: #42b983;
+    overflow: hidden;
+
+    /*overflow-y: scroll;*/
+  }
 </style>
